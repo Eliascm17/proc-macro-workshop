49e0e64b3dadcaf979a2970b3d94f6a649183acc
Eliascm17 <Elias.cmoreno17@gmail.com>
2023-07-04 09:24:07 -0500

    seq tests 1&2

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,36 @@
+extern crate proc_macro;
+
 use proc_macro::TokenStream;
+use syn::parse::{Parse, ParseStream};
+use syn::{parse_macro_input, Result, Token};
+
+#[derive(Debug)]
+struct SeqMacroInput {}
+
+impl Parse for SeqMacroInput {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let var = syn::Ident::parse(input)?;
+        let _in = <Token![in]>::parse(input)?;
+        let from = syn::Lit::parse(input)?;
+        let _dots = <Token![..]>::parse(input)?;
+        let to = syn::Lit::parse(input)?;
+        let content;
+        let braces = syn::braced!(content in input);
+
+        eprintln!(
+            "{:?} {:?} {:?} {:?} {:?} {:?}",
+            var, _in, from, _dots, to, braces
+        );
+        let ts = proc_macro2::TokenStream::parse(&content)?;
+        eprintln!("{:?}", ts);
+
+        Ok(SeqMacroInput {})
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as SeqMacroInput);
+    println!("{:?}", input);
+    TokenStream::new()
 }
